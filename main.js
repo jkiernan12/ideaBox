c6c02ce962ca834f319ce632ae15c14e1e667bcf
Delilah Necrason <delilahnecrason@gmail.com>
2021-09-15 18:12:07 -0400

    Add function to check input value

--- main.js
+++ main.js
@@ -3,19 +3,24 @@ var titleInput = document.querySelector('#titleInput');
 var bodyInput = document.querySelector('#bodyInput');
 
 saveButton.addEventListener('click', saveIdea);
+saveButton.addEventListener('mouseover', checkInputs);
+saveButton.addEventListener('mouseout', checkInputs);
 
 var ideas = [];
 
 function saveIdea() {
-  // if titleInput & bodyInput have contents, create new Idea instance.
   if (titleInput.value && bodyInput.value) {
     var currentIdea = new Idea(titleInput.value, bodyInput.value);
     ideas.push(currentIdea);
-  } else {
-    
-  }
-  // add class instance to ideas array.
+  } 
   // call render function
 };
+
+function checkInputs() {
+  if (!titleInput.value || !bodyInput.value) {
+    saveButton.classList.toggle('form__button__disabled');
+  }
+};
+
 
 // render function to render to DOM
