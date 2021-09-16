5a1ba8ae7cd7627ea8ba15c17043134ddb24fa54
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-09-16 18:13:18 -0400

    Add saving function

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var saveButton = document.querySelector('#saveButton');
 var titleInput = document.querySelector('#titleInput');
 var bodyInput = document.querySelector('#bodyInput');
-
+var ideaContainer= document.querySelector('#ideaContainer');
+window.addEventListener('load', populateIdeas);
 saveButton.addEventListener('click', saveIdea);
 saveButton.addEventListener('mouseover', checkInputs);
 saveButton.addEventListener('mouseout', checkInputs);
@@ -12,9 +13,12 @@ function saveIdea() {
   if (titleInput.value && bodyInput.value) {
     var currentIdea = new Idea(titleInput.value, bodyInput.value);
      currentIdea.saveToStorage();
-     
-  } 
+     populateIdeas();
+     titleInput.value = "";
+     bodyInput.value = "";
+  }
   // call render function
+
 };
 
 function checkInputs() {
@@ -24,12 +28,33 @@ function checkInputs() {
 };
 
 function populateIdeas() {
+  ideas = [];
   for (var i = 0; i < localStorage.length; i++){
       var currentKey = localStorage.key(i);
       var ideaToPush = localStorage.getItem(`${currentKey}`);
+      console.log(ideaToPush)
       ideaToPush = JSON.parse(ideaToPush);
       ideas.push(ideaToPush);
   }
+  render();
 }
 
 // render function to render to DOM
+function render() {
+  ideaContainer.innerHTML = "";
+  for (var i = 0; i < ideas.length; i++) {
+    ideaContainer.innerHTML +=
+`   <article class="idea-card" id='${ideas[i].id}'>
+      <div class="star-header">
+        <img class="star-header__icon" src="assets/star.svg" alt="star icon">
+        <img class="star-header__icon" src="assets/delete.svg" alt="delete icon">
+      </div>
+      <h3 class="idea-card__header">${ideas[i].title}</h3>
+      <p class="idea-card__body">${ideas[i].body}</p>
+      <div class="comment-container">
+        <input class="comment-container__add-comment" type="image" src="assets/comment.svg" width="25px">
+        <label>Comment</label>
+      </div>
+    </article>`
+  }
+}
